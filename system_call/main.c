319ffa1a47393a0b2387cd928f59596214724939
Vitorvp <vitor_vieira14@hotmail.com>
2020-08-28 23:50:05 -0300

    Create main.c
    Entrega do exercício de chamadas de sistema.

---
+++ system_call/main.c
@@ -0,0 +1,11 @@
+#include <sys/utsname.h>
+#include <stdio.h>
+
+int main(){
+	
+	struct utsname info;
+	uname(&info);
+	
+	printf("Sistema %s %s %s versao %s em %s", info.sysname, info.nodename, info.release, info.version, info.machine);
+
+}
